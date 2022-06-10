10493ec94ee9d268df6088e7f991f6eeb78e4e92
fadouaslm <fadouaslama@gmail.com>
2022-06-10 03:06:48 +0200

    restaurant done00

--- lib/database/restdata.dart
+++ lib/database/restdata.dart
@@ -51,4 +51,30 @@ class RestauService {
     return restauCollection.snapshots().map((snapshot)=> _restaurantList(snapshot));
 
   }
+
+  PartRestaurant _partRestau(DocumentSnapshot snapshot){
+    return  PartRestaurant(nom: snapshot.get("nom"), imageUrl: snapshot.get("ImageUrl"));
+  }
+
+  Stream <PartRestaurant>  partRestau (String uid){
+
+    return restauCollection.doc(uid).snapshots().map((snapshot) => _partRestau(snapshot));
+  }
+
+
+  List <Plat> _categoreList(QuerySnapshot snapshot ){
+
+    return snapshot.docs.map((doc)
+    {
+
+      return Plat(id: doc.get("ID").toString(), nom:doc.get("nom").toString(), resId: doc.get("ResId").toString(), descreption: doc.get("description").toString(), prix: doc.get("prix").toInt(), categore: doc.get("categorie"));
+
+
+    }).toList();
+
+  }
+  Stream<List <Plat>> categoreList (String uid,String categorie){
+    return restauCollection.doc(uid).collection(categorie).snapshots().map((snapshot)=> _categoreList(snapshot));
+
+  }
 }
--- lib/restaurant/restaurant.dart
+++ lib/restaurant/restaurant.dart
@@ -3,3 +3,13 @@ class Restaurant{
   final double longitude,latitude;
   Restaurant({required this.nom,required this.longitude,required this.imageUrl,required this.latitude,required this.id,required this.phone});
 }
+class PartRestaurant{
+  final String nom ,imageUrl;
+
+  PartRestaurant({required this.nom,required this.imageUrl});
+}
+class Plat{
+  final String id, resId,descreption,nom,categore;
+  final int prix;
+  Plat({required this.id,required this.nom,required this.resId,required this.descreption,required this.prix,required this.categore});
+}
